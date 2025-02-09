9be1aaf5e4ce1eb057bba0e840ad9ff35f60f8f3
Jérémie Levy <jeremielevy@mbp-de-jeremie.home>
2025-02-09 15:35:54 +0100

    NotFound view created

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import MovieDetails from '../views/MovieDetails.vue'
+// import NotFound from '../views/NotFound.vue'
 
 const routes = [
     { path: '/', component: HomePage },
     { path: '/movie/:id', component: MovieDetails, props: true },
+    // { path: '/:pathMatch(.*)*', component: NotFound }
+
 
 ]
 
