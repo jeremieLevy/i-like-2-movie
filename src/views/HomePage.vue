4d7d7f49df6edc7611ce8a29e9eb05dd86a4eaec
Jérémie Levy <jeremielevy@mbp-de-jeremie.home>
2025-02-09 18:30:56 +0100

    route to MovieDetails ok

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import MovieCard from '../components/MovieCard.vue'
+
+const route = useRoute()
 
 const apiUrl = "https://api.themoviedb.org/3/movie/popular"
 
@@ -17,26 +20,26 @@ const isLoading = ref(true)
 const errorMessage = ref('')
 
 const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTYyNmNkNmJkMGYyYWUyOGU1Y2EwOTQzMDhhYmEwZiIsIm5iZiI6MTczODk0MTYwNC4zODQsInN1YiI6IjY3YTYyNGE0NzdiOGNlZDQ1NjY3MTBiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RhaReANvMsz9dZG5Um9V_HxWwz5QPtxJNKrLIluyW8s'
-  }
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTYyNmNkNmJkMGYyYWUyOGU1Y2EwOTQzMDhhYmEwZiIsIm5iZiI6MTczODk0MTYwNC4zODQsInN1YiI6IjY3YTYyNGE0NzdiOGNlZDQ1NjY3MTBiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RhaReANvMsz9dZG5Um9V_HxWwz5QPtxJNKrLIluyW8s'
+    }
 };
 
 const getPopularMovies = async () => {
     try {
         const response = await fetch(apiUrl, options)
-        if (!response.ok) throw Error("Erreur during movies loading") 
+        if (!response.ok) throw Error("Erreur during movies loading")
 
         const data = await response.json()
-        
+
         popularMovies.value = data.results
         console.log(popularMovies);
-        
+
     } catch (error) {
         console.log("get request error")
-        errorMessage.value = (error as Error).message
+        errorMessage.value = "Failed to fetch movies"
     } finally {
         isLoading.value = false
     }
@@ -63,8 +66,8 @@ onMounted(() => getPopularMovies())
 
     <div class="relative h-150 text-white overflow-hidden mt-10">
         <div class="absolute inset-0">
-            <img src="/src/assets/Interstellar.jpg"
-                alt="Background Image" class="object-cover object-center w-full h-150" />
+            <img src="/src/assets/Interstellar.jpg" alt="Background Image"
+                class="object-cover object-center w-full h-150" />
             <div class="absolute inset-0 bg-black opacity-50"></div>
         </div>
         <div class="relative z-10 flex flex-col justify-center items-center h-150 text-center">
@@ -76,19 +79,23 @@ onMounted(() => getPopularMovies())
 
     <div class="movie-list container mx-auto p-4 md:w-165 lg:w-220 xl:w-270">
         <p v-if="isLoading">Chargement...</p>
-        <p v-else-if="errorMessage" class="text-red-500">{{ errorMessage }}</p>
+        <p v-else-if="errorMessage" class="text-red-500 text-center mt-5">{{ errorMessage }}</p>
         <div v-else class="grid gap-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-            <MovieCard v-for="movie in popularMovies.slice(0,8)" 
-            :key="movie.id"
-            :poster="movie.poster_path"
-            :title="movie.title"
-            :overview="movie.overview"
-            class="cursor-pointer"
-            />
+            <router-link 
+                v-for="movie in popularMovies.slice(0, 8)" 
+                :key="movie.id" 
+                :to="`/movie/${movie.id}`"
+                class="cursor-pointer transform transition duration-200 hover:scale-105">
+                <MovieCard 
+                    :poster="movie.poster_path" 
+                    :title="movie.title" 
+                    :overview="movie.overview"
+                    :rate-level="movie.vote_average" 
+                />
+            </router-link>
         </div>
     </div>
-    
+
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
