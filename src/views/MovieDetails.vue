1a3adc32358ba20120296761bf39d47b8a0f236c
Jérémie Levy <jeremielevy@MacBook-Pro-de-Jeremie.local>
2025-02-12 14:07:50 +0100

    display credits (actors list) on MovieDetails.vue in progress

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -4,10 +4,9 @@ import { useQuery } from "@tanstack/vue-query"
 import { getMovieDetails } from "../services/api";
 
 const route = useRoute()
-const movieId = route.params.id as string
 
+const movieId = route.params.id as string
 // const movieUrl = `https://api.themoviedb.org/3/movie/${route.params.id}&language=en-US`
-// const creditsUrl = `https://api.themoviedb.org/3/movie/${route.params.id}/credits?language=en-US`
 
 
 const { data: movie, isLoading, error } = useQuery({
@@ -63,12 +62,12 @@ const { data: movie, isLoading, error } = useQuery({
     <div v-if="movie" class="container mx-auto flex mt-30">
         <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="`${movie.original_title}`">
         <div class="flex flex-col px-8">
-            <h1>{{ movie.original_title }}</h1>
+            <h1>{{ movie.title }}</h1>
             <hr class="my-3">
             <h3 class="font-bold text-purple-300">Synopsis</h3>
             <p class="text-justify mt-3">{{ movie.overview }}</p>
             <h3 class="font-bold mt-8 text-purple-300">Casting</h3>
-            <div v-if="actors" class="mt-3 cursor-pointer">
+            <!-- <div v-if="actors" class="mt-3 cursor-pointer">
                 <ul class="inline-block" v-for="actor in actors.cast.slice(0, 6)" :key="actor.id">
                     <router-link :to="{ name: 'actor-details', params: { id: actor.id }}">
                          <li>
@@ -79,8 +78,8 @@ const { data: movie, isLoading, error } = useQuery({
                         </li>
                     </router-link>
                 </ul>
-            </div>
-            <div v-else>Chargement...</div>
+            </div> -->
+            <!-- <div v-else>Chargement...</div> -->
         </div>
     </div>
     <div v-else>Chargement...</div>
