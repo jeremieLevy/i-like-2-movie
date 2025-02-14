5d3b70604584632260caea25d69f37d6ac8bcf69
Jérémie Levy <jeremielevy@MacBook-Pro-de-Jeremie.local>
2025-02-14 17:43:23 +0100

    ESLint config ok

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { useRoute } from "vue-router";
-import { useQuery } from "@tanstack/vue-query"
+import { useRoute } from 'vue-router'
+import { useQuery } from '@tanstack/vue-query'
 
-import { getMovieDetails } from "../services/api";
-import { getMovieCredits } from '../services/api';
+import { getMovieDetails } from '../services/api'
+import { getMovieCredits } from '../services/api'
 
 import PopularMovieList from "../components/PopularMovieList.vue";
 
@@ -13,8 +13,7 @@ const route = useRoute()
 const movieId = computed(() => route.params.id as string)
 // const movieUrl = `https://api.themoviedb.org/3/movie/${route.params.id}&language=en-US`
 
-
-const { data: movie, isLoading: isLoadingMovie , error: errorMovie } = useQuery({
+const { data: movie, isLoading: isLoadingMovie, error: errorMovie } = useQuery({
     queryKey: ['movie', movieId],
     queryFn: () => getMovieDetails(movieId.value),
     staleTime: 60_000,
@@ -30,10 +29,11 @@ const { data: credits, isLoading: isLoadingCredits, error: errorCredits } = useQ
 
 </script>
 
-
 <template>
 
-    <div v-if="movie" class="container mx-auto flex mt-30">
+    <div v-if="errorMovie" class="mt-3 text-red">Something went wrong 😬</div>
+    <div v-else-if="isLoadingMovie">Loading movies...</div>
+    <div v-else-if="movie" class="container mx-auto flex mt-30">
         <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="`${movie.original_title}`" class="w-72 h-full rounded-2xl">
         <div class="flex flex-col px-8">
             <h1>{{ movie.title }}</h1>
@@ -41,10 +41,12 @@ const { data: credits, isLoading: isLoadingCredits, error: errorCredits } = useQ
             <h3 class="font-bold text-purple-300">Synopsis</h3>
             <p class="text-justify mt-3">{{ movie.overview }}</p>
             <h3 class="font-bold mt-8 texit-purple-300">Casting</h3>
-            <div v-if="credits" class="mt-3">
-                <ul class="inline-block" v-for="actor in credits.cast.slice(0,6)" :key="actor.id">
+            <div v-if="errorCredits" class="mt-3 text-red">Something went wrong 😬</div>
+            <div v-else-if="isLoadingCredits">Loading actors...</div>
+            <div v-else-if="credits" class="mt-3">
+                <ul v-for="actor in credits.cast.slice(0,6)" :key="actor.id" class="inline-block">
                     <router-link :to="{ name: 'actor-details', params: { id: actor.id }}">
-                         <li >
+                         <li>
                             <img 
                             class="w-15 h-15 rounded-full object-cover mx-2 brightness-90 hover:brightness-110" 
                             :src="`https://image.tmdb.org/t/p/w500${actor.profile_path}`" 
@@ -53,14 +55,12 @@ const { data: credits, isLoading: isLoadingCredits, error: errorCredits } = useQ
                     </router-link>
                 </ul>
             </div>
-            <div v-else>Chargement...</div>
+            <div v-else>❌ 🗒️ No credits found</div>
         </div>
     </div>
-    <div v-else>Chargement...</div>
+    <div v-else>❌ 🎥 No movies found</div>
     <PopularMovieList label="Similar movies" class="mt-10"/>
 
-
 </template>
-
 
 <style scoped></style>
